e590badffe22c26480f5a0295054169dff71e038
Aniket Pandharabale <aniketicloud@gmail.com>
2025-12-18 21:22:54 +0530

    Initial commit

---
+++ test/aws-cdk-users-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkUsersApi from '../lib/aws-cdk-users-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-users-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkUsersApi.AwsCdkUsersApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
