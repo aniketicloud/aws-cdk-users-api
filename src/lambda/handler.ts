896362d25e99c391fe8f27a727a521f806d76006
Aniket Pandharabale <aniketicloud@gmail.com>
2025-12-20 22:55:43 +0530

    add handler for retrieving a user by ID

--- src/lambda/handler.ts
+++ src/lambda/handler.ts
@@ -3,3 +3,8 @@ import { APIGatewayProxyEventV2 } from "aws-lambda";
 const getAllUsers = async (event: APIGatewayProxyEventV2) => {
   return { message: "Get All Users" };
 };
+
+const getUser = async (event: APIGatewayProxyEventV2) => { 
+  const userId = event.pathParameters?.userId;
+  return { message: `Get User with ID: ${userId}` };
+}
